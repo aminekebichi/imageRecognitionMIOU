5fffb433dc3dd99a7437f99cd66c0dad08b827c9
Amine Kebichi <amkebichi@gmail.com>
2020-09-03 14:51:23 -0400

    adding image paths and single image inference code

--- imageRecognitionDemo.py
+++ imageRecognitionDemo.py
@@ -128,3 +128,31 @@ def run_inference_for_single_image(image, graph):
       if 'detection_masks' in output_dict:
         output_dict['detection_masks'] = output_dict['detection_masks'][0]
   return output_dict
+
+#for image_path in TEST_IMAGE_PATHS:
+if (len(TEST_IMAGE_PATHS) > 1):
+  image_path = TEST_IMAGE_PATHS[0]
+  image = Image.open(join(PATH_TO_TEST_IMAGES_DIR, image_path))
+  # the array based representation of the image will be used later in order to prepare the
+  # result image with boxes and labels on it.
+  image_np = load_image_into_numpy_array(image)
+  # Expand dimensions since the model expects images to have shape: [1, None, None, 3]
+  image_np_expanded = np.expand_dims(image_np, axis=0)
+  # Actual detection.
+  output_dict = run_inference_for_single_image(image_np, detection_graph)
+  # Visualization of the results of a detection.
+  vis_util.visualize_boxes_and_labels_on_image_array(
+      image_np,
+      output_dict['detection_boxes'],
+      output_dict['detection_classes'],
+      output_dict['detection_scores'],
+      category_index,
+      instance_masks=output_dict.get('detection_masks'),
+      use_normalized_coordinates=True,
+      line_thickness=8)
+  plt.figure(figsize=IMAGE_SIZE)
+  plt.imshow(image_np)
+  #print (output_dict['detection_boxes'])
+  #print (output_dict.get('detection_masks'))
+
+  
