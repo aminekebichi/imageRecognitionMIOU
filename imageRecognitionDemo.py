469253064785659d198ddc38b9b074960dff5847
Amine Kebichi <amkebichi@gmail.com>
2020-09-03 14:23:52 -0400

    adding import statements

--- imageRecognitionDemo.py
+++ imageRecognitionDemo.py
@@ -7,6 +7,21 @@ import six.moves.urllib as urllib
 import sys
 import tensorflow as tf
 import zipfile
+from tensorflow.python.platform import gfile
+from tensorflow.core.protobuf import saved_model_pb2
+from tensorflow.python.util import compat
+from collections import defaultdict
+from io import StringIO
+from matplotlib import pyplot as plt
+from PIL import Image
+from os import listdir
+from os.path import isfile, join
+from collections import defaultdict
+from io import StringIO
+from matplotlib import pyplot as plt
+from PIL import Image
+from os import listdir
+from os.path import isfile, join
 
 print("testing")
 print("testing again")
