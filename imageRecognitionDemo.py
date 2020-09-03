62bfcab1238e8bfec9175d5ceb633a33ace6b3d1
Amine Kebichi <amkebichi@gmail.com>
2020-09-03 13:49:45 -0400

    'test'

--- imageRecognitionDemo.py
+++ imageRecognitionDemo.py
@@ -1,4 +1,11 @@
 # image detection application
 # 09/02/2020
 
+import numpy as np
+import os
+import six.moves.urllib as urllib
+import sys
+import tensorflow as tf
+import zipfile
+
 print("testing")
